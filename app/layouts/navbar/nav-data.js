4374fb70194ed5a2a68ef6415a700cb5cdbdae4a
ifeelsam <sanskarsharma9005@gmail.com>
2024-05-01 01:11:38 +0530

    repo init

---
+++ app/layouts/navbar/nav-data.js
@@ -0,0 +1,30 @@
+import config from '~/config.json';
+
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Details',
+    pathname: '/#details',
+  },
+ 
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Twitter',
+    url: `https://twitter.com/${config.twitter}`,
+    icon: 'twitter',
+  },
+  {
+    label: 'Github',
+    url: `https://github.com/${config.github}`,
+    icon: 'github',
+  },
+];
